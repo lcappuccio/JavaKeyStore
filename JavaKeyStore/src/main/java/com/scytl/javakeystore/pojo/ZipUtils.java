32ec797b0113aeecede22367547155d68e20aa5f
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-04-08 16:12:04 +0200

    add zip utils

---
+++ JavaKeyStore/src/main/java/com/scytl/javakeystore/pojo/ZipUtils.java
@@ -0,0 +1,16 @@
+/**
+ * $Id$
+ *
+ * @author lcappuccio
+ * @date 08/04/2015 16:11
+ *
+ * Copyright (C) 2015 Scytl Secure Electronic Voting SA
+ *
+ * All rights reserved.
+ *
+ */
+package com.scytl.javakeystore.pojo;
+
+public class ZipUtils {
+
+}
