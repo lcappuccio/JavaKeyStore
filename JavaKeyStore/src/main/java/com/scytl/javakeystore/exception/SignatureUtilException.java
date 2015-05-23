b4645a297b020608ea119dc238f7afcd8ab5ac4c
Leonardo Cappuccio <leo@systemexception.org>
2015-05-23 13:54:42 +0200

    remove serialVersionUID

--- JavaKeyStore/src/main/java/com/scytl/javakeystore/exception/SignatureUtilException.java
+++ JavaKeyStore/src/main/java/com/scytl/javakeystore/exception/SignatureUtilException.java
@@ -12,9 +12,7 @@
 package com.scytl.javakeystore.exception;
 
 public class SignatureUtilException extends Exception {
-	
-	private static final long serialVersionUID = 5495464303671968900L;
-	
+
     /**
      * Constructs an instance of <code>SecurityException</code> with the specified detail message.
      * @param msg the detail message.
